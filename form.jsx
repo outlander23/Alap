4eac7e359eb3711418d53b4eddd840d5b65e0197
Mahmudul Alam <mahmudula2000@gmail.com>
2020-10-01 14:08:49 +0600

    Added indention and restructured code

--- form.jsx
+++ form.jsx
@@ -1,86 +1,86 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import Input from "./input";
 
-class Form extends Component{
-	state ={
-	
-	data: {email:"",username :"",password:""},
-	errors:{}
-	
-	};
-	
-	validate=()=>{
-	const errors={};
-	if (this.state.data.username.trim()==='')
-	 errors.username="username is required"
-	if (this.state.data.password.trim()==='')
-	 errors.password="password is required"
-	return Object.keys(errors).length ===0 ? null:errors;
-	};
-	
-	validateChange = event =>{
-	let emailvalid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if (event.name === "email"){
-		     if (emailvalid.test(event.value) === false) return "Email-address is not valid";
-		    }
-	
-		if (event.name === "username"){
-			 if (event.value.trim()==="") return "Username is required";
-			 if (event.value.length<=3) return "Username is should be gater than 3 letter.";
-			}
-			
-		if (event.name === "password"){
-			 if (event.value.trim()==="") return "Password is required";
-			 if (event.value.length<=8) return "Password must be gater than 8 charecter";
-			}
-	};
-	
-	
-	handleChange=({currentTarget:event}) =>{
-		const errors={...this.state.errors};
-		const errorMassage=this.validateChange(event);
-		if (errorMassage) errors[event.name]=errorMassage;
-		else delete errors[event.name];
-		const data={...this.state.data};
-		data[event.name] =event.value;
-		this.setState({data,errors});
-	};
-	
-	handlesubmit= event=>{
-	event.preventDefault();
-	const errors=this.validate();
-	this.setState({errors:errors || {}});
-	if (errors) return;
-	
-	
-	};
-	
-	renderButton(label){
-	return (<button  
-	disabled={this.validate()} 
-	className="btn btn-primary 
+class Form extends Component {
+    // Structure of validate function is bad
+    // What happens if wan't to validate e field
+    // named "title". For this you have to edit this class.
+    // which will increase the possibility of mistake
+    // See my git Form to understand how to implement this
+    validate = () => {
+        const errors = {};
+        if (this.state.data.username.trim() === '')
+            errors.username = "username is required"
+        if (this.state.data.password.trim() === '')
+            errors.password = "password is required"
+        return Object.keys(errors).length === 0 ? null : errors;
+    };
+    // Too many useless use of "event" variable. Use Object Destructuring
+    // See ES6 refresher(React-Mosh video) to know about Obj. Dest..
+    // Structure is bad as validate function
+    validateChange = ({name, value}) => {
+        // removed / from      here          here
+        let emailvalid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if (name === "email") {
+            if (emailvalid.test(value) === false) return "Email-address is not valid";
+        }
+
+        if (name === "username") {
+            if (value.trim() === "") return "Username is required";
+            if (value.length <= 3) return "Username is should be gater than 3 letter.";
+        }
+
+        if (name === "password") {
+            if (value.trim() === "") return "Password is required";
+            if (value.length <= 8) return "Password must be gater than 8 charecter";
+        }
+    };
+
+
+    handleChange = ({currentTarget: event}) => {
+        const errors = {...this.state.errors};
+        const errorMassage = this.validateChange(event);
+        if (errorMassage) errors[event.name] = errorMassage;
+        else delete errors[event.name];
+        const data = {...this.state.data};
+        data[event.name] = event.value;
+        this.setState({data, errors});
+    };
+
+    // name should be in CamelCase
+    handleSubmit = event => {
+        event.preventDefault();
+        const errors = this.validate();
+        this.setState({errors: errors || {}});
+        if (errors) return;
+
+
+    };
+
+    renderButton(label) {
+        return (<button
+            disabled={this.validate()}
+            className="btn btn-primary
 	">
-	{label}
-	</button>);
-	};
-	
-	renderInput(name,label,type){
-	
-	const{data,errors}=this.state;
-	
-	return (
-	<Input 
-	placeholder={label}
-	name={name}
-	value={data[name]}
-	label={label}
-	onChange={this.handleChange}
-	type={type}
-	error={errors[name]}
-	/>
-	);
-	}
-	
+            {label}
+        </button>);
+    };
+
+    renderInput(name, label, type) {
+        const {data, errors} = this.state;
+        return (
+            <Input
+                placeholder={label}
+                name={name}
+                value={data[name]}
+                label={label}
+                onChange={this.handleChange}
+                type={type}
+                error={errors[name]}
+            />
+        );
+    }
+
 }
-	
+
 export default Form;
--- loginForm.jsx
+++ loginForm.jsx
@@ -1,38 +1,31 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-import Form from "./form";
+import Form from "./common/form";
 
 
-
-class Login extends Form{
-
-	
-
-	
-	render(){
-	
-	return (
-	<div className="container">
-	
-	
-	<h2>Login</h2>
-	
-	<form onSubmit={this.handlesubmit}>
-	
-	{this.renderInput("email","Email","text")}
-	{this.renderInput("username","Username","text")}
-	{this.renderInput("password","Password","password")}
-	{this.renderButton("login")}
-	
-	
-	</form>
-
-	</div>
-	);
-	
-	
-	 
-		
-	}
+class Login extends Form {
+    // Use State here. Because you don't
+    // Want to edit Form to reduce mistake.
+    // Every editing may lead to a bug
+    state = {
+        data: {email: "", username: "", password: ""},
+        errors: {}
+    };
+    // Use indention Like python
+    // Without indention it is hard to read
+    render() {
+        return (
+            <div className="container">
+                <h2>Login</h2>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("email", "Email", "text")}
+                    {this.renderInput("username", "Username", "text")}
+                    {this.renderInput("password", "Password", "password")}
+                    {this.renderButton("login")}
+                </form>
+            </div>
+        );
+    }
 }
+
 export default Login;
